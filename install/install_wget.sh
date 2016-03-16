9c81152062924cd98fa273814417d3c3820b9961
Joel McCracken <mccracken.joel@gmail.com>
2016-03-16 05:29:34 -0400

    rm a bunch of junk install/remove files
    these are all soooo ancient

--- install/install_wget.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# requires brew
-
-brew install wget
--- install/remove_xcode.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "ERROR: No good easy solution for this available anywhere; check the internets"
-echo "(so xcode wasn't removed)"
-exit 1
